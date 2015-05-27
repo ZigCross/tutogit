285d6db4606b8b2dc4e2a4a350a16e6057bd6458
kisaragui <njesusas@gmail.com>
2015-05-27 09:38:23 -0430

    27/05/2015 ,,,, cambios integracion de la funcion de leer la cadena alfanumerica

--- archivo.py
+++ archivo.py
@@ -3,19 +3,7 @@
 # parte del programa que esta crea el archivo
 # "texto.txt" y los guarda en el archivo creado
 
-print " BUENOS DIAS !!!!! "
-print "PROGRAMITA SENSILL0"
-
-def guardardatos():
+def guardardatos(datos):
 	archivo= open("texto.txt", "a")
-	print"ingrese datos a guardar: "
-	datos = raw_input()
-	print "datos guardados: " + datos
 	archivo.write(datos +"\n")
 	archivo.close
-
-
-
-
-guardardatos()
-
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+from archivo import guardardatos
+
 salir = False
 
 while not salir:
 	opcion = raw_input('Seleccione una opcion: ')
 
 	if opcion == '1':
-		print "Bloque de la opcion 1"
+		passwd = raw_input('Ingrese una contrasena alfanumerica en minusculas: ')
+		# Aqui va el codigo de ramses
+		if funcion_ramses(passwd):
+			guardardatos(passwd)
+
 	elif opcion == '2':
 		print "Bloque de la opcion 2"
 	elif opcion == '3':
